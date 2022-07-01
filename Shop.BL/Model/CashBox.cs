3f72b3f248815eb2830b1a0ddad8e7eeca967253
Yury <semerikov.y@gmail.com>
2022-07-01 14:36:42 +0500

    касса

---
+++ Shop.BL/Model/CashBox.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.BL.Model
+{
+    public class CashBox
+    {
+        /// <summary>
+        /// Номер кассы.
+        /// </summary>
+        public int Number { get; set; }
+        /// <summary>
+        /// Продавец на кассе.
+        /// </summary>
+        public Seller Seller { get; set; }
+        /// <summary>
+        /// Очередь на кассе.
+        /// </summary>
+        public Queue<Cart> Queue { get; set; }
+        /// <summary>
+        /// Максимальная длина очереди.
+        /// </summary>
+        public int MaxLength { get; set; } = 10;
+        /// <summary>
+        /// Счетчик покупателей, ушедших без покупки.
+        /// </summary>
+        public int ExitCustomer { get; set; }
+        public bool IsModel { get; set; }
+
+        private ShopContext context = new ShopContext();
+        public CashBox(int number, Seller seller)
+        {
+            Number = number;
+            Seller = seller;
+            Queue = new Queue<Cart>();
+            IsModel = true;
+        }
+
+        public void Enqueue(Cart cart)
+        {
+            if (Queue.Count < MaxLength)
+            {
+                Queue.Enqueue(cart);
+            }
+            else
+            {
+                ExitCustomer++;
+            }
+        }
+
+        public decimal Dequeue()
+        {
+            decimal result = 0;
+            var cart = Queue.Dequeue();
+            if (cart != null)
+            {
+                var check = new Check();
+                check.Seller = this.Seller;
+                check.SellerId = this.Seller.SellerId;
+                check.Customer = cart.Customer;
+                check.CustomerId = cart.Customer.CustomerId;
+                check.Created = DateTime.Now;
+
+
+                if (!IsModel)
+                {
+                    context.Checks.Add(check);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    check.CheckId = 0;
+                }
+
+                var sells = new List<Sell>();
+                foreach (Product product in cart)
+                {
+                    if (product.Count > 0)
+                    {
+                        var sell = new Sell
+                        {
+                            CheckId = check.CheckId,
+                            Check = check,
+                            ProductId = product.ProductId,
+                            Product = product
+                        };
+                        sells.Add(sell);
+                        if (!IsModel)
+                        {
+                            context.Sells.Add(sell);
+                        }
+                        product.Count--;
+                        result += product.Price;
+                    }
+                }
+                if (!IsModel)
+                {
+                    context.SaveChanges();
+                }
+            }
+            return result;
+        }
+
+
+
+
+
+        ~CashBox()
+        {
+            context.Dispose();
+        }
+
+    }
+}
---
+++ Shop.BLTests/Model/CashBoxTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shop.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.BL.Model.Tests
+{
+    [TestClass()]
+    public class CashBoxTests
+    {
+        [TestMethod()]
+        public void CashBoxTest()
+        {
+            //Arrange
+            var customer1 = new Customer()
+            {
+                Name = "test customer 1",
+                CustomerId = 1
+            };
+            var customer2 = new Customer()
+            {
+                Name = "test customer 2",
+                CustomerId = 2
+            };
+            var seller = new Seller()
+            {
+                Name = "test seller",
+                SellerId = 1
+            };
+
+            var product1 = new Product("test product 1", 100, "test unit 1", 10);
+            product1.ProductId = 1;
+            var product2 = new Product("test product 2", 200, "test unit 2", 20);
+            product2.ProductId = 2;
+            var cart1 = new Cart(customer1);
+            var cart2 = new Cart(customer2);
+            var cashBox = new CashBox(1, seller);
+
+            var cart1ExpectedResult = 500;
+            var cart2ExpectedResult = 600;
+
+            //Act
+            cart1.Add(product1);
+            cart1.Add(product1);
+            cart1.Add(product2);
+            cart1.Add(product1);
+            cart2.Add(product2);
+            cart2.Add(product1);
+            cart2.Add(product1);
+            cart2.Add(product2);
+            cashBox.Enqueue(cart1);
+            cashBox.Enqueue(cart2);
+            var cart1ActualResult = cashBox.Dequeue();
+            var cart2ActualResult = cashBox.Dequeue();
+            //Assert
+            Assert.AreEqual(cart1ActualResult, cart1ExpectedResult);
+            Assert.AreEqual(cart2ActualResult, cart2ExpectedResult);
+            Assert.AreEqual(5, product1.Count);
+            Assert.AreEqual(17, product2.Count);
+        }
+
+        
+    }
+}
